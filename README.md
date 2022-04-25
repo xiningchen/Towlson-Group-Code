be523dd0897a0fa153a42c6c1f30dd86d4773748
Xining Chen <shine@Xinings-MacBook-Pro-M1.local>
2022-04-25 12:38:22 -0600

    Updating louvain community library to use bctpy

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ See 'example_demo.ipynb' for examples of using functions in python files
 * Function for averaging brain connectomes
 * Function for getting basic network statistics (WIP)
 
+### Resources/References 
+* [Brain Connectivity Toolbox (Python)](https://pypi.org/project/bctpy/)
+* [Community Louvain 2021 - MATLAB](https://drive.google.com/drive/folders/1P32DAUy1AFEn7biMomD0v8j373byRAOq)
+* [ARC Cluster Guide](https://github.com/mariamasoliver/connect_to_ARC/blob/main/Guide.md)
--- functional_brain_community.py
+++ functional_brain_community.py
@@ -1,72 +1,74 @@
-# !pip install python-louvain
 from collections import Counter
-
 import networkx as nx
 import numpy as np
 from matplotlib import pyplot as plt
 from tqdm import tqdm
-from community import community_louvain
+# from community import community_louvain
+import bct
 import matplotlib.cm as cm
 from sklearn.metrics.cluster import adjusted_rand_score
+import math
 
-
-def find_resolution_range(G, min_community_num, max_community_num):
+def find_resolution_range(W, min_community_num, max_community_num, min_gamma, max_gamma, inc, B='modularity', sample_size=1):
     x_axis = []
     y_axis = []
     good_res = []
-    for r in tqdm(range(0, 300)):
-        res = 0.1+r*0.01
+    max_range = math.ceil((max_gamma - min_gamma) / inc)
+    for r in tqdm(range(0, max_range)):
+        res = min_gamma + r * inc
         x_axis.append(res)
-        c = community_louvain.best_partition(G, resolution=res, randomize=True)
-        if len(Counter(c.values())) >= min_community_num and len(Counter(c.values())) <= max_community_num:
-            avg_num = len(Counter(c.values()))
-            for i in range(0,100):
-                c = community_louvain.best_partition(G, resolution=res, randomize=True)
-                avg_num += len(Counter(c.values()))
-            avg_num = avg_num/100
+        # c = community_louvain.best_partition(G, resolution=res, randomize=True)
+        c, q = bct.community_louvain(W, gamma=res, B=B)
+        avg_num = len(Counter(c))
+        if avg_num >= min_community_num and avg_num <= max_community_num:
+            for i in range(0,sample_size):
+                c, q = bct.community_louvain(W, gamma=res, B=B)
+                avg_num += len(Counter(c))
+            avg_num = avg_num/sample_size
             y_axis.append(avg_num)
             if avg_num >= min_community_num and avg_num <= max_community_num:
                 good_res.append((res, avg_num))
         else:
-            y_axis.append(len(Counter(c.values())))
+            y_axis.append(len(Counter(c)))
     return good_res, x_axis, y_axis
 
 
-def community_by_modularity_stability(G, count, res_upper_range, res_lower_range):
-    partition_by_gamma = {r: [] for r in range(0, res_upper_range)}
-    partition_list_by_gamma = {r: [] for r in range(0, res_upper_range)}
-    stability_by_gamma = {r: 0.0 for r in range(0, res_upper_range)}
-    modularity_by_gamma = {r: 0.0 for r in range(0, res_upper_range)}
-    for i in tqdm(range(0, 500)):
-        for r in range(0, res_upper_range):
-            res = res_lower_range + (0.005) * r
-            partition = community_louvain.best_partition(G, resolution=res, randomize=True)
-            partition_list = [p for nlab, p in partition.items()]
+def community_by_modularity_stability(W, res_lower_range, res_upper_range, inc, B='modularity', rep=2):
+    count = math.comb(rep, 2)
+    r_upper = math.ceil((res_upper_range - res_lower_range)/inc)
+    partition_by_gamma = {r: [] for r in range(0, r_upper)}
+    partition_list_by_gamma = {r: [] for r in range(0, r_upper)}
+    stability_by_gamma = {r: 0.0 for r in range(0, r_upper)}
+    modularity_by_gamma = {r: 0.0 for r in range(0, r_upper)}
+    for i in tqdm(range(0, rep)):
+        for r in range(0, r_upper):
+            res = res_lower_range + inc * r
+            # partition = community_louvain.best_partition(G, resolution=res, randomize=True)
+            partition, q = bct.community_louvain(W, gamma=res, B=B)
+            partition_list = [p for p in partition]
             if len(partition_by_gamma[r]) >= 1:
                 # Compute a z-rand score
                 for plist in partition_list_by_gamma[r]:
                     stability_by_gamma[r] += adjusted_rand_score(plist, partition_list)
-                    if r == 0:
-                        count += 1
             partition_by_gamma[r].append(partition)
             partition_list_by_gamma[r].append(partition_list)
-            # Compute modularity value
-            modularity_by_gamma[r] += community_louvain.modularity(partition, G)
+            # Compute modularity value  ****** is q returned modularity value? Or something q-statistc??
+            modularity_by_gamma[r] += q
     # Calculate average z-rand score per gamma
     avg_stab_by_gamma = np.array([z / count for r, z in stability_by_gamma.items()])
     # Calculate average modularity score per gamma
-    avg_modularity_by_gamma = np.array([mod / 500 for r, mod in modularity_by_gamma.items()])
+    avg_modularity_by_gamma = np.array([mod / rep for r, mod in modularity_by_gamma.items()])
     # Calculate weighted avg z-rand = <mod>*<z-rand> per gamma
     weighted_avg_zscore = avg_modularity_by_gamma * avg_stab_by_gamma
     # Find max(weighted avg z-rand) --> gamma_max
     index_max = np.argmax(weighted_avg_zscore)
     if weighted_avg_zscore[index_max] != max(weighted_avg_zscore):
         print("Max gamma incorrect - something wrong.")
-    return res_lower_range+0.005*index_max, index_max, avg_modularity_by_gamma, avg_stab_by_gamma, partition_by_gamma
+    return res_lower_range + inc*index_max, index_max, avg_modularity_by_gamma, avg_stab_by_gamma, partition_by_gamma
 
 
 def draw_community(G, partition):
-    print(community_louvain.modularity(partition, G))
+    # print(community_louvain.modularity(partition, G))
     pos = nx.spring_layout(G)
     cmap = cm.get_cmap('viridis', max(partition.values()) + 1)
     nx.draw_networkx_nodes(G, pos, partition.keys(), node_size=40, cmap=cmap, node_color=list(partition.values()))
